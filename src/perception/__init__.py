5d2a1248f0cc2cd10182cf3a3182f4934537fa63
Boris <boris.cheng@epitech.eu>
2026-01-25 14:22:28 +0100

    feat: perception init

--- src/perception/__init__.py
+++ src/perception/__init__.py
@@ -7,3 +7,63 @@ Components:
 - SensorFusion: Fuse multiple sensors for state estimation
 - Transforms: Coordinate transformations
 """
+
+from .lidar_processor import (
+    LidarProcessor,
+    ProcessedScan,
+    ScanSegment,
+    create_simulated_scan
+)
+
+from .obstacle_detector import (
+    ObstacleDetector,
+    Obstacle,
+    ObstacleType,
+    BoundingBox
+)
+
+from .sensor_fusion import (
+    SensorFusion,
+    RobotState,
+    SensorReading,
+    SensorType,
+    SensorStatus
+)
+
+from .transforms import (
+    Pose2D,
+    Transform2D,
+    FrameManager,
+    GPSConverter,
+    normalize_angle,
+    angle_difference
+)
+
+__all__ = [
+    # LiDAR
+    'LidarProcessor',
+    'ProcessedScan',
+    'ScanSegment',
+    'create_simulated_scan',
+
+    # Obstacles
+    'ObstacleDetector',
+    'Obstacle',
+    'ObstacleType',
+    'BoundingBox',
+
+    # Fusion
+    'SensorFusion',
+    'RobotState',
+    'SensorReading',
+    'SensorType',
+    'SensorStatus',
+
+    # Transforms
+    'Pose2D',
+    'Transform2D',
+    'FrameManager',
+    'GPSConverter',
+    'normalize_angle',
+    'angle_difference'
+]
